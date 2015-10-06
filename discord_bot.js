2c87a83f098116139e0a5b4644f712b06fbf7262
Alex <alex.chaldyshev@gmail.com>
2015-10-05 22:17:07 -0400

    adds gif bot. blatantly rips off: https://github.com/WritheM/Wallace/blob/master/plugins/giphy/giphy.js

--- discord_bot.js
+++ discord_bot.js
@@ -8,6 +8,14 @@ var Discord = require("discord.js");
 
 // Get the email and password
 var AuthDetails = require("./auth.json");
+var qs = require("querystring");
+
+var config = {
+    "api_key": "dc6zaTOxFJmzC",
+    "rating": "pg-13",
+    "url": "http://api.giphy.com/v1/gifs/random",
+    "permission": ["NORMAL"]
+};
 
 var bot = new Discord.Client();
 
@@ -23,6 +31,25 @@ bot.on("disconnected", function () {
 });
 
 bot.on("message", function (msg) {
+	if(msg.content.substring(0, 4) === "!gif"){
+		var tags = msg.content.split(" ");
+		tags.shift();
+		//bot.sendMessage(msg.channel, tags);
+		get_gif(tags, function (id) {
+            if (typeof id !== "undefined") {
+
+           		bot.sendMessage(msg.channel, "http://media.giphy.com/media/" + id + "/giphy.gif [Tags: " + (tags ? tags : "Random GIF") + "]");
+            }
+            else {
+            	bot.sendMessage(msg.channel, "Invalid tags, try something different. [Tags: " + (tags ? tags : "Random GIF") + "]");
+            }
+        });
+
+
+	}
+
+
+
 	if (msg.content.substring(0, 4) === "ping") {
 		
 		//send a message to the channel the ping message was sent in.
@@ -33,5 +60,29 @@ bot.on("message", function (msg) {
 
 	}
 });
+
+function get_gif(tags, func) {
+        var params = {
+            "api_key": config.api_key,
+            "rating": config.rating,
+            "format": "json"
+        };
+        if (tags !== null) {
+            params.tag = tags;
+        }
+
+        var query = qs.stringify(params);
+//wouldnt see request lib if defined at the top for some reason:\
+var request = require("request");
+
+        request(config.url + "?" + query, function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                console.error("giphy: Got error: " + body);
+            }
+            else {
+                func(JSON.parse(body).data.id);
+            }
+        }.bind(this));
+    }
 
 bot.login(AuthDetails.email, AuthDetails.password);
