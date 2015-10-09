18e734639f18c8ff531e4e144009a5bcdbe4eb31
Einarin <kc0itv@gmail.com>
2015-10-09 00:29:24 -0400

    converted pullanddeploy to be native

--- discord_bot.js
+++ discord_bot.js
@@ -127,12 +127,36 @@ var commands = {
     "pullanddeploy": {
         description: "bot will perform a git pull master and restart with the new code",
         process: function(bot,msg,suffix) {
-            bot.sendMessage(msg.channel,"brb!",function(error,sentMsg){
+            bot.sendMessage(msg.channel,"fetching updates...",function(error,sentMsg){
                 console.log("updating...");
 	            var spawn = require('child_process').spawn;
-                spawn('sh', [ 'pullanddeploy.sh' ]).on("close",function(code){
-                    console.log("exiting");
-                    process.exit();
+                var log = function(err,stdout,stderr){
+                    if(stdout){console.log(stdout);}
+                    if(stderr){console.log(stderr);}
+                };
+                var fetch = spawn('git', ['fetch']);
+                fetch.stdout.on('data',function(data){
+                    console.log(data.toString());
+                });
+                fetch.on("close",function(code){
+                    var reset = spawn('git', ['reset','--hard','origin/master']);
+                    reset.stdout.on('data',function(data){
+                        console.log(data.toString());
+                    });
+                    reset.on("close",function(code){
+                        var npm = spawn('npm', ['install']);
+                        npm.stdout.on('data',function(data){
+                            console.log(data.toString());
+                        });
+                        npm.on("close",function(code){
+                            console.log("goodbye");
+                            bot.sendMessage(msg.channel,"brb!",function(){
+                                bot.logout(function(){
+                                    process.exit();
+                                });
+                            });
+                        });
+                    });
                 });
             });
         }
