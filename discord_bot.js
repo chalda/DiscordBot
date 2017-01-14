60683d66ea04cca399e229f89f3d0f80f7adc568
Bacon_Space <thestreamstats@gmail.com>
2017-01-14 17:18:37 -0500

    updated The SetGame Code
    ```JS
    bot.user.setStatus("online", Config.commandPrefix + "help | " + bot.guilds.array().length + " Servers");
    ```

--- discord_bot.js
+++ discord_bot.js
@@ -751,7 +751,7 @@ bot.on("ready", function () {
 	console.log("Logged in! Serving in " + bot.guilds.array().length + " servers");
 	require("./plugins.js").init();
 	console.log("type "+Config.commandPrefix+"help in Discord for a commands list.");
-	bot.user.setStatus("online",Config.commandPrefix+"help");
+	bot.user.setStatus("online", Config.commandPrefix + "help | " + bot.guilds.array().length + " Servers");
 });
 
 bot.on("disconnected", function () {
