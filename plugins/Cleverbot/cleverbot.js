ac7d71340b9b1afd6108adc2798356519768304f
Bacon_Space <thestreamstats@gmail.com>
2017-01-18 13:42:41 -0500

    Update cleverbot.js

--- plugins/Cleverbot/cleverbot.js
+++ plugins/Cleverbot/cleverbot.js
@@ -1,28 +1,28 @@
 exports.commands = [
-	"talk"
+    "talk"
 ]
 
 var cleverbot = require("cleverbot-node");
 talkbot = new cleverbot;
-cleverbot.prepare(function(){});
+cleverbot.prepare(function() {});
 
 exports.talk = {
-	usage : "<message>",
-	description : "Talk directly to the bot",
-	process : function(bot,msg, suffix) {
-			var conv = suffix.split(" ");
-			talkbot.write(conv, function (response) {
-			msg.channel.sendMessage("", {
-          embed: {
-              color: 0x8698FE,
-              author: {
-                  name: msg.author.username,
-                  icon_url: msg.author.avatarURL
-              },
-              description: response.message,
-          }
-      }).catch(console.error);
-			msg.react('👌');
-			})
-	}
+    usage: "<message>",
+    description: "Talk directly to the bot",
+    process: function(bot, msg, suffix) {
+        var conv = suffix.split(" ");
+        talkbot.write(conv, function(response) {
+            msg.channel.sendMessage("", {
+                embed: {
+                    color: 0x8698FE,
+                    author: {
+                        name: msg.author.username,
+                        icon_url: msg.author.avatarURL
+                    },
+                    description: response.message,
+                }
+            }).catch(console.error);
+            msg.react('👌');
+        })
+    }
 }
