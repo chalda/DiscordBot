7b705e46e5c628ebc078bd478e094e45e535c13a
bleonard252 <bleonard252@rocketmail.com>
2017-04-11 15:06:19 -0400

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -131,7 +131,8 @@ var commands = {
             }
 	    if (msg.channel.id == "300808172371836928") {
 		msg.channel.sendMessage("@here The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.");
-        }
+            }
+	}
     },
     "rip": {
         description: "Rests something in pepperoni",
