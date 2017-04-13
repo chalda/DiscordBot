d44bb468a63ae1b09690f6ba823e2f7f8e01b8de
bleonard252 <bleonard252@rocketmail.com>
2017-04-13 09:51:55 -0400

    Moveover process.

--- plugins/Admin/admin.js
+++ plugins/Admin/admin.js
@@ -1,7 +1,11 @@
 exports.commands = [
 	"setUsername",
 	"log",
-	"uptime"
+	"uptime",
+	"alias",
+	"aliases",
+	"idle",
+	"online"
 ]
 
 var startTime = Date.now();
@@ -49,3 +53,53 @@ exports.uptime = {
 		msg.channel.sendMessage("**Uptime**: " + timestr);
 	}
 }
+
+	exports.alias = {
+		usage: "<name> <actual command>",
+		description: "Creates command aliases. Useful for making simple commands on the fly",
+		process: function(bot,msg,suffix) {
+			var args = suffix.split(" ");
+			var name = args.shift();
+			if(!name){
+				msg.channel.sendMessage(Config.commandPrefix + "alias " + this.usage + "\n" + this.description);
+			} else if(commands[name] || name === "help"){
+				msg.channel.sendMessage("overwriting commands with aliases is not allowed!");
+			} else {
+				var command = args.shift();
+				aliases[name] = [command, args.join(" ")];
+				//now save the new alias
+				require("fs").writeFile("./alias.json",JSON.stringify(aliases,null,2), null);
+				msg.channel.sendMessage("created alias " + name);
+			}
+		}
+	}
+
+	exports.aliases = {
+		description: "lists all recorded aliases",
+		process: function(bot, msg, suffix) {
+			var text = "current aliases:\n";
+			for(var a in aliases){
+				if(typeof a === 'string')
+					text += a + " ";
+			}
+			msg.channel.sendMessage(text);
+		}
+	}
+
+    exports.idle = {
+	usage: "[status]",
+        description: "sets bot status to idle",
+        process: function(bot,msg,suffix){ 
+	    bot.user.setStatus("idle");
+	    bot.user.setGame(suffix);
+	}
+    }
+
+    exports.online = {
+	usage: "[status]",
+        description: "sets bot status to online",
+        process: function(bot,msg,suffix){ 
+	    bot.user.setStatus("online");
+	    bot.user.setGame(suffix);
+	}
+    }
