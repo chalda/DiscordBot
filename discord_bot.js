a6d304519ab3d84ac9738e9a4f7b43a66d120972
bleonard252 <bleonard252@rocketmail.com>
2017-04-13 09:15:11 -0400

    Attempt to allow web access.
    yeeeee

--- discord_bot.js
+++ discord_bot.js
@@ -2,7 +2,48 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
- var meth = http.createServer()
+ var meth = http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+ })
  meth.listen(process.env.PORT);
 
  console.log('Server running at http://127.0.0.1:5000/');
