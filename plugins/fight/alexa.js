1add41528b7d08f94098107861e0c382662f607b
bleonard252 <bleonard252@rocketmail.com>
2019-04-22 19:35:38 -0400

    begin the fight

--- plugins/fight/alexa.js
+++ plugins/fight/alexa.js
@@ -1,13 +1,33 @@
+const Discord = require("discord.js");
 exports.commands = [
-	"alexa"
+	"fight"
 ]
 
-//alexa sdk
-
-exports.alexa = {
-	usage: "<request...>",
-	description: "ask alexa something",
+exports.fight = {
+	usage: "<challengee> [challengee]...",
+	description: "fite some1",
 	process: function(bot,msg,suffix){
-		/*for now*/ msg.channel.send("Alexa is not yet available. Try again later.")
+		let primitivefighters = suffix.split(" ");
+		let fighters = [];
+		for (let fighter in primitivefighters) {
+			if (fighter instanceof Discord.User) fighters.push(fighter);
+		}
+		//msg.channel.send("Alexa is not yet available. Try again later.")
+		if (fighters) {
+			let xrandr = crypto.randomBytes(3).toString('hex');
+			msg.channel.guild.createChannel("ftmpf"+xrandr,"text")
+			.then(function(channel) {
+				channel.overwritePermissions(msg.channel.guild.defaultRole,{"SEND_MESSAGES":false,"VIEW_CHANNEL":true,"READ_MESSAGES":true,"ADD_REACTIONS":false});
+				channel.overwritePermissions(msg.author,{"SEND_MESSAGES":true,"VIEW_CHANNEL":true,"READ_MESSAGES":true});
+				for (fighter in fighters) {
+					channel.overwritePermissions(fighter,{"SEND_MESSAGES":true,"VIEW_CHANNEL":true,"READ_MESSAGES":true});
+				};
+            	msg.channel.send("created " + channel);
+				if (fighters.length = 1) channel.setTopic("Fight between " + msg.author + " and " + fighters[0] "; facilitated by " + bot);
+				if (fighters.length > 1) channel.setTopic("Fight between " + msg.author + ", " + fighters.join(", ") + "; facilitated by " + bot);
+			}).catch(function(error){
+				msg.channel.send("failed to create channel: " + error);
+			})
+		}
 	}
 }
