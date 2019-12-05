adfb0a090c104fe48e380ab802f7e8714b9697dc
BikeMaker <BikeMaker@users.noreply.github.com>
2019-12-05 01:21:07 -0500

    Update stock.js to use tmxmoney
    Quick and dirty modification using axios, cheerio, and string-sanitizer to make the stock plugin work via tmxmoney.com's quote lookup.

--- plugins/Stock/stock.js
+++ plugins/Stock/stock.js
@@ -1,22 +1,32 @@
+var string = require("string-sanitizer");
+var axios = require('axios');
+var cheerio = require('cheerio');
+var url = 'https://web.tmxmoney.com/quote.php?qm_symbol=';
+
 exports.commands = [
-	"stock"
+        "stock"
 ]
 
 exports.stock = {
-	usage: "<stock to fetch>",
-	process: function(bot,msg,suffix) {
-		var yahooFinance = require('yahoo-finance');
-		yahooFinance.snapshot({
-		  symbol: suffix,
-		  fields: ['s', 'n', 'd1', 'l1', 'y', 'r'],
-		}, function (error, snapshot) {
-			if(error){
-				msg.channel.send("couldn't get stock: " + error);
-			} else {
-				//msg.channel.send(JSON.stringify(snapshot));
-				msg.channel.send(snapshot.name
-					+ "\nprice: $" + snapshot.lastTradePriceOnly);
-			}
-		});
-	}
+        usage: "<stock to fetch>",
+        process: function(bot, msg, suffix) {
+                suffix = string.sanitize(suffix);
+                var qurl = url + suffix;
+                axios.get(qurl).then(response => {
+                        if(response.status === 200) {
+                                var html = response.data;
+                                var $ = cheerio.load(html);
+                                var price = $('.price > span').text()
+                                console.log(suffix + " price: $" + price);
+                                msg.channel.send(suffix + " price: $" + price);
+                        } else {
+                                console.log("error fetching quote.\nStatus: " + response.status);
+                                msg.channel.send("Error fetching quote.");
+                        }
+                })
+                .catch(error => {
+                        console.log(error);
+                        msg.channel.send("Error completing request for stock quote.")
+                });
+        }
 }
