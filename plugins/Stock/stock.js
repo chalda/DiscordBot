32a0c0036ae936d9d254df469c69a3f405c6684d
William Dickinson <45810504+william-dickinson@users.noreply.github.com>
2019-12-24 02:20:52 -0700

    Updated stock.js

--- plugins/Stock/stock.js
+++ plugins/Stock/stock.js
@@ -7,8 +7,8 @@ exports.commands = [
 ]
 
 exports.stock = {
-	usage: "<ticker>",
-	description: "Returns a stock price for a given ticker. Stonks.",
+	usage: "<ticker>[,<ticker>,...]",
+	description: "Returns a stock price for a given ticker. Example: !stock TSLA,BRK.A,PHIA.AS",
 	process: function(bot,msg,suffix){
 	let stock_api = "https://api.worldtradingdata.com/api/v1/stock?symbol="
 		request({
@@ -19,8 +19,26 @@ exports.stock = {
             if(result.Message){
                 msg.channel.send(result.Message);
             } else {
-                msg.channel.send(result.data[0].name+" $"+result.data[0].price);
-              }
+                result.data.forEach(stock => {
+                  let price = new Intl.NumberFormat("en-US",{ style: 'currency', currency: stock.currency}).format(stock.price);
+                  let market_cap = new Intl.NumberFormat("en-US",{ style: 'currency', currency: stock.currency}).format(stock.market_cap);
+                  let eps = new Intl.NumberFormat("en-US",{ style: 'currency', currency: stock.currency}).format(stock.eps);
+                  msg.channel.send("", {
+                    embed: {
+                      title: stock.name,
+                      description:
+                        "__**Price: " + price + "**__\n" +
+                        "Market Cap " + market_cap + "\n" + 
+                        "Earnings Per Share: " + eps,
+                      url: "https://www.worldtradingdata.com/stock/" + stock.symbol,
+                      footer: {
+                        "text": stock.stock_exchange_long
+                      }
+                    }
+                  });
+              })
+            }
+      
 		});
 	}
 }
