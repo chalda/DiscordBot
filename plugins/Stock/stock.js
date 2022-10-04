778dc471f3f0763fb63e1aa1e19225bd70137840
Nic <43260193+nic-w@users.noreply.github.com>
2022-10-04 06:54:44 -0700

    Stock fix (#223)
    * updated stock cmd to use finage api
    
    * upper case the tickers in msg embed
    
    Co-authored-by: Johnny Dickinson <kc0itv@gmail.com>

--- plugins/Stock/stock.js
+++ plugins/Stock/stock.js
@@ -1,80 +1,75 @@
 var AuthDetails = require("../../auth.js").getAuthDetails();
-// Stock command disabled because both variants are broken :(
-/*exports.commands = [
-	"stock"
-]*/
+exports.commands = ["stock"];
 
-if(AuthDetails.worldtradingdata_api_key) {
-	var request = require("request");
-	var Discord = require("discord.js");
+const formatStockPrice = (num) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(num);
+};
 
-	exports.stock = {
-		usage: "<ticker>[,<ticker>,...]",
-		description: "Returns a stock price for a given ticker. Example: !stock TSLA,BRK.A,PHIA.AS",
-		process: function(bot,msg,suffix){
-		let stock_api = "https://api.worldtradingdata.com/api/v1/stock?symbol="
-			request({
-				url: stock_api+suffix+"&api_token="+AuthDetails.worldtradingdata_api_key
-			},
-		  function(err,res,body){
-				console.log(body)
-				let result = JSON.parse(body)
-				if(result.Message){
-					msg.channel.send(result.Message);
-				} else if(result.message) {
-					msg.channel.send(result.message);
-				} else {
-					result.data.forEach(stock => {
-					  let price = new Intl.NumberFormat("en-US",{ style: 'currency', currency: stock.currency}).format(stock.price);
-					  let market_cap = new Intl.NumberFormat("en-US",{ style: 'currency', currency: stock.currency}).format(stock.market_cap);
-					  let eps = new Intl.NumberFormat("en-US",{ style: 'currency', currency: stock.currency}).format(stock.eps);
-					  msg.channel.send("", {
-						embed: {
-						  title: stock.name,
-						  description:
-							"__**Price: " + price + "**__\n" +
-							"Market Cap " + market_cap + "\n" + 
-							"Earnings Per Share: " + eps,
-						  url: "https://www.worldtradingdata.com/stock/" + stock.symbol,
-						  footer: {
-							"text": stock.stock_exchange_long
-						  }
-						}
-					  });
-				  })
-				}
-      
-			});
-		}
-	}
+const parseResponseAndSend = (stock, channel) => {
+  const { price, symbol, timestamp } = stock;
+  const formattedPrice = formatStockPrice(price);
+
+  channel.send("", {
+    embed: {
+      title: symbol.toUpperCase(),
+      description: `__**Price: ${formattedPrice}**__\n`,
+      url: "https://finage.co.uk/stock/" + symbol,
+    },
+  });
+};
+
+if (AuthDetails.finage_api_key) {
+  var request = require("request");
+  var Discord = require("discord.js");
+
+  exports.stock = {
+    usage: "<ticker>[,<ticker>,...]",
+    description:
+      "Returns a stock price for a given ticker. Example: !stock TSLA",
+    process: function (bot, msg, suffix) {
+      if (!suffix) {
+        msg.channel.send(this.description);
+        return;
+      }
+      let stock_api_url = "https://api.finage.co.uk";
+      stock_api_url += suffix.includes(",")
+        ? `/last/trade/stocks?symbols=${suffix}&`
+        : `/last/trade/stock/${suffix}?`;
+      stock_api_url += `apikey=${AuthDetails.finage_api_key}`;
+      request(
+        {
+          url: stock_api_url,
+        },
+        function (err, res, body) {
+          console.log(body);
+          let result = JSON.parse(body);
+          if (result.error) {
+            msg.channel.send(result.error);
+          } else if (result.success === false) {
+            msg.channel.send(
+              `Error Status ${result.statusCode}: ${result.message}`
+            );
+          } else {
+            if (result.length) {
+              for (let stock of result) {
+                parseResponseAndSend(stock, msg.channel);
+              }
+            } else {
+              parseResponseAndSend(result, msg.channel);
+            }
+          }
+        }
+      );
+    },
+  };
 } else {
-	var string = require('string-sanitizer');
-	var axios = require('axios');
-	var cheerio = require('cheerio');
-	var tmxmoney_url = 'https://money.tmx.com/en/quote/tsla';
-
-	exports.stock = {
-			usage: "<stock to fetch>",
-			process: function(bot, msg, suffix) {
-					suffix = string.sanitize(suffix);
-					var qurl = tmxmoney_url + suffix + ":US";
-					axios.get(qurl).then(response => {
-							if(response.status === 200) {
-									var html = response.data;
-									console.log(html);
-									var $ = cheerio.load(html);
-									var price = $('.price > span').text()
-									console.log(suffix + " price: $" + price);
-									msg.channel.send(suffix + " price: $" + price);
-							} else {
-									console.log("error fetching quote.\nStatus: " + response.status);
-									msg.channel.send("Error fetching quote. Set up an API key for better stocks!");
-							}
-					})
-					.catch(error => {
-							console.log(error);
-							msg.channel.send("Error completing request for stock quote.");
-					});
-			}
-	}
+  exports.stock = {
+    usage: "<stock to fetch>",
+    process: function (bot, msg, suffix) {
+      msg.channel.send("API Key has not been set up yet");
+    },
+  };
 }
